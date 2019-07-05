9a19dad4be294b05fe3a38bef6e2dfb2c4ca2d7f
caron yan <carony@163.com>
2019-07-05 15:58:24 +0800

    feat(layout): layout style ctrl

--- src/components/layout/AppContainer.vue
+++ src/components/layout/AppContainer.vue
@@ -1,7 +1,11 @@
 <template>
-    <section>
-        <div>
-            <transition>
+    <section class="main-container" :style="[hideSidebar]">
+        <div class="main-content">
+            <transition 
+                mode="out-in"
+                enter-active-class="fadeIn"
+                leave-active-class="fadeOut"
+                appear>
                 <router-view></router-view>
             </transition>
         </div>
@@ -9,7 +13,28 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-    
+    computed: {
+        ...mapGetters({
+            sidebar: 'sidebar'
+        }),
+        hideSidebar() {
+            return this.sidebar.opened ? null : { 'margin-left': 0 }
+        }
+    }    
 }
 </script>
+
+<style lang="scss">
+.main-container {
+    padding-top: 50px;
+    margin-left: 180px;
+    transform: translate3d(0, 0, 0);
+}
+
+.main-content {
+    padding: 20px;
+}
+</style>
