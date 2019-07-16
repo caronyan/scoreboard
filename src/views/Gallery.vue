52aa8a811930e013289ebd94a3c7b7c9decb547c
caron yan <carony@163.com>
2019-07-16 17:52:17 +0800

    feat(view): gallery init

---
+++ src/views/Gallery.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <carousel></carousel>  
+    </div>
+</template>
+
+<script>
+import Carousel from '@/components/Carousel.vue'
+
+export default {
+    name: 'gallery',
+    components: {
+        Carousel
+    }
+}
+</script>
