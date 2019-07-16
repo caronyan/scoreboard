a4cd71de3c5cb4133e738fae9ecd76b6b5ccf65c
caron yan <carony@163.com>
2019-07-16 16:21:11 +0800

    feat(com): init carousel

---
+++ src/components/Carousel.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <ol class="carousel-indicator">
+            <li v-for="i in indicators" @click="indicator($index)" v-bind:key="i" v-bind:class="{ active: $index === index }"></li>
+        </ol>
+        <div class="carousel-container">
+            <slot></slot>
+        </div>
+        <div class="carousel-control">
+            <a href=""></a>
+            <a href=""></a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            index: 0,
+            indicators: [],
+            isSliding: false
+        }
+    },
+    methods: {
+        indicatorHandler(index) {
+            if (this.isSliding || this.index === index) {
+                return false
+            }
+            this.isSliding = true
+            this.index = index
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
