fa7d43cc407cd28d7c7b879ec5fced28ef0cc9d8
caron yan <carony@163.com>
2019-07-21 16:16:05 +0800

    feat(view): delete template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <ace-test></ace-test>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-import AceTest from '@/components/AceEditor.vue'
 
 export default {
   name: 'home',
   components: {
-    AceTest
   }
 }
 </script>
