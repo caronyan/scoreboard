fa3a057ecd2bc266b88420722653dc1eb71de9d3
caron yan <carony@163.com>
2019-07-23 16:37:09 +0800

    feat(view): test carousel style

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -2,10 +2,14 @@
     <div>
         <agile>
             <div class="slide">
-                slide 1
+                <img alt="Vue logo" src="@/assets/logo.png">
             </div>
-            <div class="slide">slide 2</div>
-            <div class="slide">slide 3</div>    
+            <div class="slide">
+                <img alt="Vue logo" src="@/assets/logo.png">
+            </div>
+            <div class="slide">
+                <img alt="Vue logo" src="@/assets/logo.png">    
+            </div>    
         </agile>  
     </div>
 </template>
@@ -20,3 +24,67 @@ export default {
     }
 }
 </script>
+
+<style lang='scss'>
+.agile {
+    &__actions {
+        margin-top: 20px;
+    }
+
+	&__nav-button {
+        background: transparent;
+		border: none;
+		color: #ccc;
+		cursor: pointer;
+		font-size: 24px;
+		transition-duration: .3s;
+
+		&:hover {
+            color: #888;
+        }
+    }
+		
+	&__dot {
+        margin: 0 10px;
+
+		button {
+            background-color: #eee;
+			border: none;
+			border-radius: 50%;
+			cursor: pointer;
+			display: block;
+			height: 10px;
+			font-size: 0;
+			line-height: 0;
+			margin: 0;
+			padding: 0;
+			transition-duration: .3s;
+			width: 10px;
+        }
+			
+		&--current,
+		&:hover {
+            button {
+                background-color: #888;
+            }
+        }
+    }
+		
+}
+	
+
+// Slides styles	
+.slide {
+    align-items: center;
+	color: #fff;
+	display: flex;
+	height: 300px;
+	justify-content: center;
+
+	h3 {
+        font-size: 32px;
+		font-weight: 300;
+    }
+}
+	
+</style>
